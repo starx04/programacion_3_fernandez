fb609b8f028b910cbd3899d348ade538d5f898ea
starx04 <antonifernandez207@gmail.com>
2026-07-02 09:02:31 -0500

    Clase - 2-jul-2026

--- modulo4/03-use-Reducer/src/App.tsx
+++ modulo4/03-use-Reducer/src/App.tsx
@@ -2,7 +2,7 @@
 
 import BasicCounter     from './components/BasicCounter'
 import RegistrationForm from './components/RegistrationForm'
-/*import ShoppingCart     from './components/ShoppingCart'*/
+import ShoppingCart     from './components/ShoppingCart'
 
 // ┌──────────────────────────────────────────────────────────────────────┐
 // │  Cambia PASO y guarda (Ctrl+S) para navegar entre componentes.      │
@@ -15,8 +15,8 @@ const PASO = 1
 export default function App() {
   const content =
     PASO === 1 ? <BasicCounter /> :
-    PASO === 2 ? <RegistrationForm /> :/*
-    PASO === 3 ? <ShoppingCart /> :*/
+    PASO === 2 ? <RegistrationForm /> :
+    PASO === 3 ? <ShoppingCart /> :
     <p style={{ color: '#e00' }}>Paso {PASO}: crea el componente primero</p>
 
   return (
--- modulo4/03-use-Reducer/src/components/RegistrationForm.tsx
+++ modulo4/03-use-Reducer/src/components/RegistrationForm.tsx
@@ -4,22 +4,25 @@ import { useReducer } from 'react'
 
 interface FormState {
   name:     string
+  last_name:     string
   email:    string
   password: string
-  errors:   Partial<Record<'name' | 'email' | 'password', string>>
-  status:   'idle' | 'submitting' | 'success' | 'error'
+  errors:   Partial<Record<'name' | 'last_name' | 'email' | 'password', string>>
+  status:   'idle' | 'submitting' | 'success' | 'error'| 'validating'
 }
 
 type FormAction =
-  | { type: 'SET_FIELD'; field: keyof Pick<FormState, 'name' | 'email' | 'password'>; value: string }
+  | { type: 'SET_FIELD'; field: keyof Pick<FormState, 'name' | 'last_name' | 'email' | 'password'>; value: string }
   | { type: 'SET_ERRORS'; errors: FormState['errors'] }
   | { type: 'SUBMIT_START' }
+  | { type: 'SUBMIT_VALIDATING' }
   | { type: 'SUBMIT_SUCCESS' }
   | { type: 'SUBMIT_ERROR' }
   | { type: 'RESET' }
 
 const INITIAL_STATE: FormState = {
   name:     '',
+  last_name:     '',
   email:    '',
   password: '',
   errors:   {},
@@ -43,6 +46,8 @@ function formReducer(state: FormState, action: FormAction): FormState {
       return { ...INITIAL_STATE, status: 'success' }
     case 'SUBMIT_ERROR':
       return { ...state, status: 'error' }
+    case 'SUBMIT_VALIDATING':
+      return { ...state, status: 'validating' }
     case 'RESET':
       return INITIAL_STATE
   }
@@ -54,6 +59,7 @@ export default function RegistrationForm() {
   function validate(): boolean {
     const errors: FormState['errors'] = {}
     if (!state.name.trim())         errors.name     = 'El nombre es requerido'
+    if (!state.last_name.trim())    errors.last_name = 'El apellido es requerido'
     if (!state.email.includes('@')) errors.email    = 'Email inválido'
     if (state.password.length < 6)  errors.password = 'Mínimo 6 caracteres'
 
@@ -69,12 +75,16 @@ export default function RegistrationForm() {
     if (!validate()) return
 
     dispatch({ type: 'SUBMIT_START' })
+    // Simulación de Validacion
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    dispatch({ type: 'SUBMIT_VALIDATING' })
     // Simulación de llamada a API
     await new Promise((resolve) => setTimeout(resolve, 1200))
     dispatch({ type: 'SUBMIT_SUCCESS' })
   }
 
   const isSubmitting = state.status === 'submitting'
+  const isValidating = state.status === 'validating'
 
   return (
     <form
@@ -93,7 +103,7 @@ export default function RegistrationForm() {
           onChange={(e) =>
             dispatch({ type: 'SET_FIELD', field: 'name', value: e.target.value })
           }
-          placeholder="Nombre completo"
+          placeholder="Nombre"
           disabled={isSubmitting}
           style={inputStyle(!!state.errors.name)}
         />
@@ -101,7 +111,20 @@ export default function RegistrationForm() {
           <p style={errorStyle}>{state.errors.name}</p>
         )}
       </div>
-
+        <div>
+        <input
+          value={state.last_name}
+          onChange={(e) =>
+            dispatch({ type: 'SET_FIELD', field: 'last_name', value: e.target.value })
+          }
+          placeholder="Apellido"
+          disabled={isSubmitting}
+          style={inputStyle(!!state.errors.last_name)}
+        />
+        {state.errors.last_name && (
+          <p style={errorStyle}>{state.errors.last_name}</p>
+        )}
+      </div>
       <div>
         <input
           type="email"
@@ -140,13 +163,13 @@ export default function RegistrationForm() {
           disabled={isSubmitting}
           style={{
             flex: 1, padding: '10px',
-            background: isSubmitting ? '#93c5fd' : '#0070f3',
+            background: isSubmitting || isValidating ? '#93c5fd' : '#0070f3',
             color: '#fff', border: 'none', borderRadius: 6,
-            cursor: isSubmitting ? 'not-allowed' : 'pointer',
+            cursor: isSubmitting || isValidating ? 'not-allowed' : 'pointer',
             fontWeight: 500,
           }}
         >
-          {isSubmitting ? 'Registrando...' : 'Registrar'}
+          {isSubmitting ? 'Registrando...' : isValidating ? 'Validando...' : 'Registrar'}
         </button>
         <button
           type="button"
