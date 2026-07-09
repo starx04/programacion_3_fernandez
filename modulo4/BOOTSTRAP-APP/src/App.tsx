345c79bcebcb69a0a8a36d3bc112e4fb6eb80377
starx04 <antonifernandez207@gmail.com>
2026-07-09 09:00:10 -0500

    clase 09-jun-2026

---
+++ modulo4/BOOTSTRAP-APP/src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+
+import AppHome from './AppHome'
+
+
+export default function App() {
+  //return <AppLab />
+  // Fase 2 — descomenta y comenta la línea anterior:
+  return (
+    <>
+  <AppHome />
+  </>
+  )
+}
+
